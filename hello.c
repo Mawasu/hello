dc1c64e3c25059c541d7d883a6b46634e7f34e95
Cale Sigerson <csigerson2@huskers.unl.edu>
2023-08-24 11:11:01 -0500

    initial commit

---
+++ hello.c
@@ -0,0 +1,17 @@
+/**
+ * Author: Cale Sigerson
+ *         EMAIL
+ * Date: 2023/08/24
+ *
+ * A simple hello world program in C
+ *
+ */
+#include <stdlib.h>
+#include <stdio.h>
+
+int main(int argc, char **argv) {
+
+  printf("Cale Sigerson\n");
+
+  return 0;
+}
